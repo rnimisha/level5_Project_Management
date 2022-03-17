489d8b92130300e6b4ccf25ac68543c66df34900
rnimisha <nimisaraut@gmail.com>
2022-03-18 00:21:43 +0545

    name validation draft

--- code/registerCustomer.php
+++ code/registerCustomer.php
@@ -9,21 +9,37 @@
     <title>User Registration</title>
 </head>
 <body>
+    <div id="reg-sucess-msg" style="color: green"></div>
+
     <form action="validateCustomer.php" method="POST" id="cust-reg-form">
         <label for="fullname"> Name</label>
-        <input type="text" name="uname" id="fullname" /><br/><br/>
+        <input type="text" name="uname" id="fullname" /><br/>
+        <div id="name_error" style="color: red"></div>
+
         <label for="useremail"> Email</label>
-        <input type="text" name="uemail" id="useremail" /><br/><br/>
+        <input type="text" name="uemail" id="useremail" /><br/>
+        <div id="email_error" style="color: red"></div>
+
         <label for="pword"> Password</label>
-        <input type="password" name="upass" id="pword" /><br/><br/>
+        <input type="password" name="upass" id="pword" /><br/>
+        <div id="pass_error" style="color: red"></div>
+
         <label for="repass"> Re-enter Password</label>
-        <input type="text" name="urep-pass" id="repass" /><br/><br/>
+        <input type="text" name="urep-pass" id="repass" /><br/>
+        <div id="repass_error" style="color: red"></div>
+
         <label for="contact"> Contact</label>
-        <input type="text" name="ucontact" id="contact" /><br/><br/>
+        <input type="text" name="ucontact" id="contact" /><br/>
+        <div id="contact_error" style="color: red"></div>
+
         <label for="dob"> DOB</label>
-        <input type="text" name="udob" id="dob" /><br/><br/>
+        <input type="text" name="udob" id="dob" /><br/>
+        <div id="dob_error" style="color: red"></div>
+
         <label for="address"> Address</label>
-        <input type="text" name="uaddress" id="address" /><br/><br/>
+        <input type="text" name="uaddress" id="address" /><br/>
+        <div id="address_error" style="color: red"></div>
+
         <!-- image later -->
         <input type="submit" name="registerCustomer" value="Register"/>
     </form>
--- code/validateCustomer.php
+++ code/validateCustomer.php
@@ -1,4 +1,35 @@
 <?php
 include 'connection.php';
-echo json_encode(0);
+
+$error=array();
+$error['clear']=false;
+
+//validation
+if(isset($_POST['fullname']))
+{
+    //name validation
+    if(!empty(trim($_POST['fullname'])))
+    {
+        if(strlen($_POST['fullname']) < 4)
+        {
+            $error['#name_error']="Enter a valid name";
+            $error['clear']=false;
+
+        }
+        else{
+            $error['#name_error']="";
+            $error['clear']=true;
+            $fullname=mysqli_real_escape_string($connection, $_POST['fullname']);
+
+        }
+    }
+    else{
+        $error['#name_error']="Name cannot be empty";
+        $error['clear']=false;
+
+    }
+}
+
+//response
+echo json_encode($error);
 ?>
