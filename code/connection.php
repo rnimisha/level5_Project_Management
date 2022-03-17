f2f2ec808330f908992172bc3691bb2518dba512
rnimisha <nimisaraut@gmail.com>
2022-03-17 23:15:05 +0545

    mysql connection for testing

---
+++ code/connection.php
@@ -0,0 +1,14 @@
+<?php
+$hostname="localhost";
+$username="root";
+$password="";
+$database="phoenix_mart";
+
+$connection=mysqli_connect($hostname,$username,$password,$database);
+
+if($connection)
+{
+    echo "connection success";
+}
+
+?>
