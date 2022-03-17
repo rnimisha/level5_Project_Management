8a313bef4f6e898bf583abcdb1d45fa36988da9b
rnimisha <nimisaraut@gmail.com>
2022-03-18 00:34:30 +0545

    email validation started

--- code/validateCustomer.php
+++ code/validateCustomer.php
@@ -5,7 +5,7 @@ $error=array();
 $error['clear']=false;
 
 //validation
-if(isset($_POST['fullname']))
+if(isset($_POST['fullname']) && isset($_POST['useremail']))
 {
     //name validation
     if(!empty(trim($_POST['fullname'])))
@@ -27,6 +27,35 @@ if(isset($_POST['fullname']))
         $error['#name_error']="Name cannot be empty";
         $error['clear']=false;
 
+    }
+
+    //email validation
+    if(!empty(trim($_POST['useremail']))){
+        if(filter_var($_POST['useremail'], FILTER_VALIDATE_EMAIL)){
+
+            //sanitize before rurnning query
+            $useremail=mysqli_real_escape_string($connection, $_POST['useremail']);
+            $checkQuery="SELECT * FROM `user` WHERE email='$useremail'";
+            $result=mysqli_query($connection,$checkQuery);
+            if(mysqli_num_rows($result)>0){
+                $error['#email_error']="Enter already registered";
+                $error['clear']=false;
+            }
+
+            $error['#email_error']="";
+            $error['clear']=true;
+
+
+        }
+        else{
+            $error['#email_error']="Enter a valid email";
+            $error['clear']=false;
+        }
+
+    }
+    else{
+        $error['#email_error']="Email cannot be empty";
+        $error['clear']=false;
     }
 }
 
