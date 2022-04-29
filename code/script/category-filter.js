b56ce1f9c62b651f7fb4d9b02ce1cc66f11f69be
rnimisha <nimisaraut@gmail.com>
2022-04-29 08:56:23 +0545

    range slider changed

--- code/script/category-filter.js
+++ code/script/category-filter.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
 
-    //jquery function for price range
+    //jquery ui function for price range
     $(function() {
         $("#price-range").slider({
-          step: 1,
-          range: true, 
-          min: 0, 
-          max: 1000, 
-          values: [0, 1000], 
-          slide: function(event, ui)
-          {$("#priceRange").val('\u00A3' +ui.values[0] + " - " +'\u00A3' + ui.values[1]);}
+            step: 1,
+            range: true, 
+            min: 0, 
+            max: 1000, 
+            values: [0, 1000], 
+            slide: function(event, ui)
+            {
+                $("#min-price").text(ui.values[0]);
+                $("#max-price").text(ui.values[1]);
+            }
         });
-        $("#priceRange").val('\u00A3' + $("#price-range").slider("values", 0) + " - " + '\u00A3'+ $("#price-range").slider("values", 1));
-        
+        $("#min-price").text($("#price-range").slider("values", 0));
+        $("#max-price").text($("#price-range").slider("values", 1));
     });
 
+    //show cartand wishlist option on hover
     $(".cat-product").hover(function(){
         $(this).find('.option-container').css("visibility", "visible");
         $(this).find('.option-container2').css("visibility", "visible");
