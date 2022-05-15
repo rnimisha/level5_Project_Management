d729b9449491f7689e339695586b6d57518aec52
rnimisha <nimisaraut@gmail.com>
2022-05-15 15:03:09 +0545

    category homepage added

--- index.php
+++ index.php
@@ -21,58 +21,10 @@ include_once('function.php');
 </head>
 
 <body>
-  <div class="container-fluid p-0 header-main" id="sticky-nav">
-    <nav class="navbar py-0 navbar-expand-lg navbar-light border-bottom">
-      <a class="navbar-brand pl-5" href="index.php" id="logo-header">
-        <img src="image\logo.png" alt="logo" />
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto ml-5">
-          <li class="nav-item">
-            <a class="nav-link " href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link  ml-3" href="category-page.php">Shop</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link  ml-3" href="about-us.php">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link ml-3" href="contact-us-page.php">Contact</a>
-          </li>
-        </ul>
-        <div class="justify-content-right navbar-nav search-bar transition-effect d-none ">
-          <form class="form-inline ml-auto" id="text-filter">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" id="ftext" name="ftext">
-            <button class="btn d-none btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-        <div class="pr-5 nav-logo text-right">
-          <span class="mr-3 search-icon transition-effect">
-            <ion-icon name="search-outline"></ion-icon>
-          </span>
-          <span class="mr-3 user-hover">
-            <ion-icon name="person-outline"></ion-icon>
-          </span>
-          <a href="wishlist-page.php"><span class="mr-3">
-              <ion-icon name="heart-outline"></ion-icon></i>
-            </span></a>
-          <a href="cart-page.php"><span class="mr-3">
-              <ion-icon name="cart-outline"></ion-icon>
-            </span></a>
-        </div>
-      </div>
-    </nav>
-  </div>
-
+  <?php include_once('header.php');?>
   <!-- banner -->
   <div class="container mt-5 pt-5">
-    <div class="row mt-3">
+    <div class="row mt-4">
       <div class="col-lg-4 d-lg-block d-none">
         <div>
           <div class="img-container">
@@ -98,6 +50,142 @@ include_once('function.php');
         </div>
       </div>
     </div>
+
+
+    <div class="row mt-5 mb-5 d-flex justify-content-center align-items-center main-category">
+    <!-- <div class="col-12 text-center h3 my-green-font mb-2">
+        Category
+      </div> -->
+      <div class="col-md-2 col-4 d-block text-center">
+        <img src="image\category\fruit.png" alt="profile" class="img-fluid category-img" />
+        <div>Fruit</div>
+      </div>
+      <div class="col-md-2 col-4  text-center">
+        <img src="image\category\butcher.png" alt="profile" class="img-fluid category-img" />
+        <div>Meat</div>
+      </div>
+      <div class="col-md-2 col-4  text-center">
+        <img src="image\category\bakery.png" alt="profile" class="img-fluid category-img" />
+        <div>Bakery</div>
+      </div>
+      <div class="col-md-2 col-4 text-center">
+        <img src="image\category\fish.png" alt="profile" class="img-fluid category-img" />
+        <div>Seafood</div>
+      </div>
+      <!-- <div class="col-md-2 col-4">
+        <img src="image\category\deli.png" alt="profile" class="img-fluid category-img" />
+        <div class="ml-4">Fruit</div>
+      </div> -->
+      <div class="col-md-2 col-4 text-center">
+        <img src="image\category\allcat.png" alt="profile" class="img-fluid category-img" />
+        <div>All</div>
+      </div>
+    </div>
+
+    
+
+
+    <!-- best sellers -->
+    <div class="row mt-5 mb-5 top-seller">
+      <div class="col-12 text-center mt-5 h3 my-green-font">
+        Top Seller
+      </div>
+      <div class="row px-5 pb-5">
+        <?php
+
+          $query="SELECT a.* FROM (SELECT COUNT(P.PRODUCT_ID) AS COUNT_ORDER, PRODUCT_NAME, PRICE, P.PRODUCT_ID AS PRODUCT_ID FROM PRODUCT P JOIN ORDER_ITEM OI ON P.PRODUCT_ID=OI.PRODUCT_ID GROUP BY P.PRODUCT_ID, PRODUCT_NAME, PRICE ORDER BY COUNT(P.PRODUCT_ID) DESC)a WHERE ROWNUM <= 6";
+          $parsed=oci_parse($connection, $query);
+          oci_execute($parsed);
+          while(($row = oci_fetch_assoc($parsed)) != false) 
+          {
+          ?>
+        <div class="col-lg-4 col-sm-6 mt-4 cat-product-container " value="<?php echo $row['PRODUCT_ID'];?>">
+          <div class="cat-product col-12 text-center top-seller-product">
+            <div class="inner-img-container">
+              <img src="image\product\<?php echo(getProductImage($row['PRODUCT_ID'],$connection)[0]); ?>"
+                class="img-fluid product-pic" alt="product-img" />
+            </div>
+            <div class="option-container d-flex">
+              <div>
+                <i class='bx bx-search-alt-2 quick-view-product' value="<?php echo $row['PRODUCT_ID'];?>"></i>
+              </div>
+              <div>
+                <i class='bx bx-cart-alt add-to-cart' value="<?php echo $row['PRODUCT_ID'];?>"></i>
+              </div>
+              <div>
+                <?php
+                            if(isset($_SESSION['phoenix_user']) && $_SESSION['user_role'])
+                            {
+                                $wishlist_status=checkProductInWishList($row['PRODUCT_ID'], $_SESSION['phoenix_user'], $connection);
+
+                                if($wishlist_status)
+                                {
+                                ?>
+                <i class='bx bxs-heart remove-from-wishlist' value="<?php echo $row['PRODUCT_ID'] ?>"></i>
+                <?php
+                                }
+                                else
+                                {
+                                ?>
+                <i class='bx bx-heart save-to-wishlist' value="<?php echo $row['PRODUCT_ID'] ?>"></i>
+                <?php
+                                }
+                            }
+                            else{
+                                ?>
+                <i class='bx bx-heart save-to-wishlist' value="<?php echo $row['PRODUCT_ID'] ?>"></i>
+                <?php
+                            }
+                            ?>
+              </div>
+            </div>
+            <div>
+              <!-- display rating for product -->
+              <?php 
+                            $avgRating=getAvgRating($row['PRODUCT_ID'], $connection);
+                            for($i=1; $i<=$avgRating; $i++)
+                            {
+                                ?>
+              <i class='bx bxs-star'></i>
+              <?php
+                            }
+                            for($i=1; $i<=(5-$avgRating); $i++)
+                            {
+                                ?>
+              <i class='bx bx-star'></i>
+              <?php
+                            }
+                        ?>
+            </div>
+            <div>
+              <b><?php echo $row['PRODUCT_NAME']; ?></b>
+            </div>
+            <div>
+              <?php 
+                        $dis_rate=checkProductDiscountRate($row['PRODUCT_ID'], $connection);
+                        if($dis_rate>0){
+                            ?>
+              <span
+                class="prod-price"><?php echo calculatePriceWithDiscount($row['PRODUCT_ID'], $connection);?></span>&nbsp;
+              <span class="before-discount">&#163;<?php echo $row['PRICE']; ?> </span>
+              <?php
+                        }
+                        else{
+                            ?>
+              <span class="prod-price"><?php echo $row['PRICE']; ?></span>
+              <?php
+                        }
+                    ?>
+            </div>
+          </div>
+        </div>
+        <?php 
+                }
+            ?>
+      </div>
+    </div>
+
+
 
     <!-- New arrivals -->
     <div class="row mt-5 mb-5">
@@ -199,114 +287,25 @@ include_once('function.php');
       </div>
     </div>
 
+    
 
-    <!-- best sellers -->
-    <div class="row mt-5 mb-5 top-seller">
-      <div class="col-12 text-center mt-5 h3">
-        Top Seller
-      </div>
-      <div class="row px-5 pb-5">
-        <?php
-
-          $query="SELECT a.* FROM (SELECT COUNT(P.PRODUCT_ID) AS COUNT_ORDER, PRODUCT_NAME, PRICE, P.PRODUCT_ID AS PRODUCT_ID FROM PRODUCT P JOIN ORDER_ITEM OI ON P.PRODUCT_ID=OI.PRODUCT_ID GROUP BY P.PRODUCT_ID, PRODUCT_NAME, PRICE ORDER BY COUNT(P.PRODUCT_ID) DESC)a WHERE ROWNUM <= 6";
-          $parsed=oci_parse($connection, $query);
-          oci_execute($parsed);
-          while(($row = oci_fetch_assoc($parsed)) != false) 
-          {
-          ?>
-        <div class="col-lg-4 col-sm-6 mt-4 cat-product-container " value="<?php echo $row['PRODUCT_ID'];?>">
-          <div class="cat-product col-12 text-center top-seller-product">
-            <div class="inner-img-container">
-              <img src="image\product\<?php echo(getProductImage($row['PRODUCT_ID'],$connection)[0]); ?>"
-                class="img-fluid product-pic" alt="product-img" />
-            </div>
-            <div class="option-container d-flex">
-              <div>
-                <i class='bx bx-search-alt-2 quick-view-product' value="<?php echo $row['PRODUCT_ID'];?>"></i>
-              </div>
-              <div>
-                <i class='bx bx-cart-alt add-to-cart' value="<?php echo $row['PRODUCT_ID'];?>"></i>
-              </div>
-              <div>
-                <?php
-                            if(isset($_SESSION['phoenix_user']) && $_SESSION['user_role'])
-                            {
-                                $wishlist_status=checkProductInWishList($row['PRODUCT_ID'], $_SESSION['phoenix_user'], $connection);
-
-                                if($wishlist_status)
-                                {
-                                ?>
-                <i class='bx bxs-heart remove-from-wishlist' value="<?php echo $row['PRODUCT_ID'] ?>"></i>
-                <?php
-                                }
-                                else
-                                {
-                                ?>
-                <i class='bx bx-heart save-to-wishlist' value="<?php echo $row['PRODUCT_ID'] ?>"></i>
-                <?php
-                                }
-                            }
-                            else{
-                                ?>
-                <i class='bx bx-heart save-to-wishlist' value="<?php echo $row['PRODUCT_ID'] ?>"></i>
-                <?php
-                            }
-                            ?>
-              </div>
-            </div>
-            <div>
-              <!-- display rating for product -->
-              <?php 
-                            $avgRating=getAvgRating($row['PRODUCT_ID'], $connection);
-                            for($i=1; $i<=$avgRating; $i++)
-                            {
-                                ?>
-              <i class='bx bxs-star'></i>
-              <?php
-                            }
-                            for($i=1; $i<=(5-$avgRating); $i++)
-                            {
-                                ?>
-              <i class='bx bx-star'></i>
-              <?php
-                            }
-                        ?>
-            </div>
-            <div>
-              <b><?php echo $row['PRODUCT_NAME']; ?></b>
-            </div>
-            <div>
-              <?php 
-                        $dis_rate=checkProductDiscountRate($row['PRODUCT_ID'], $connection);
-                        if($dis_rate>0){
-                            ?>
-              <span
-                class="prod-price"><?php echo calculatePriceWithDiscount($row['PRODUCT_ID'], $connection);?></span>&nbsp;
-              <span class="before-discount">&#163;<?php echo $row['PRICE']; ?> </span>
-              <?php
-                        }
-                        else{
-                            ?>
-              <span class="prod-price"><?php echo $row['PRICE']; ?></span>
-              <?php
-                        }
-                    ?>
-            </div>
-          </div>
-        </div>
-        <?php 
-                }
-            ?>
-      </div>
-    </div>
-
-    <!-- banner -->
+    <!-- two side banner -->
     <div class="row p-0 d-flex justify-content-center align-items-center">
-      <div class="img-container col-lg-6 pl-0">
+      <div class="img-container col-lg-6 pr-2 pl-0">
         <img src="image\banner\longbanner.jpg" class="img-fluid category-banner" alt="banner">
+        <div class="banner-text col-5">
+            <h3>Healthy meat</h3>
+            <p class="text-dark">Top quality meat for you</p>
+            <a href="category-page.php?submit-filter=&category[]=3" class="btn px-3">Shop</a>
+        </div>
       </div>
-      <div class="img-container col-lg-6 d-lg-flex d-none pr-0">
+      <div class="img-container col-lg-6 d-lg-flex d-none pl-2 pr-0">
         <img src="image\banner1.png" class="img-fluid category-banner" alt="banner">
+        <div class="banner-text col-5">
+            <h3>Rich in quality</h3>
+            <p class="text-dark">Locallly grown juciest fruits</p>
+            <a href="category-page.php?submit-filter=&category[]=1" class="btn px-3">Shop</a>
+        </div>
       </div>
       <!-- <img src="image\banner\longbanner.jpg" alt="payment success" class="no-data-found img-fluid" /> -->
     </div>
@@ -418,39 +417,59 @@ include_once('function.php');
   <div class="container-fluid">
 
   <div class="row mt-5 mb-5 p-0">
-    <div class="col-12 p-0">
+    <div class="col-12 p-0 fullbanner-container">
       <img src="image\banner\full_longbanner.jpg" alt="payment success" class="full-banner img-fluid" />
+      <div class="full-banner-text col-5">
+            <h2>100% Organic and Fresh!</h2>
+            <p>Beverages full of nutrition and energy</p>
+            <a href="category-page.php?submit-filter=&category[]=5" class="btn px-3">Shop</a>
+    </div>
     </div>
   </div>
+
     <!-- OUR TRADERS -->
-    <div class="row mt-5 mb-5 d-flex justify-content-center align-items-center">
+    <!-- <div class="row mt-5 mb-5 d-flex justify-content-center align-items-center">
       <div class="col-12 text-center h3 my-green-font mb-2">
         Our Traders
       </div>
       <div class="col-md-2 col-4">
-        <img src="image\shop\Traders-butcher.jpg" alt="profile" class="img-fluid trader-img" />
+        <img src="image\cat\butcher.jpeg" alt="profile" class="img-fluid trader-img" />
       </div>
       <div class="col-md-2 col-4">
         <img src="image\shop\Traders-fishmonger.jpg" alt="profile" class="img-fluid trader-img" />
       </div>
       <div class="col-md-2 col-4">
-        <img src="image\shop\Traders-greengrocer.jpg" alt="profile" class="img-fluid trader-img" />
+        <img src="image\cat\fruit3.jpg" alt="profile" class="img-fluid trader-img" />
       </div>
       <div class="col-md-2 col-4">
-        <img src="image\shop\Traders-bakery.jpg" alt="profile" class="img-fluid trader-img" />
+        <img src="image\cat\bakery.jpg" alt="profile" class="img-fluid trader-img" />
       </div>
       <div class="col-md-2 col-4">
         <img src="image\shop\Traders-delicatessen.jpg" alt="profile" class="img-fluid trader-img" />
       </div>
     </div>
+ -->
 
 
-
-    <div class="row bg-light">
-      <div class="col-7 mx-auto text-center mt-5">
-        <img src="image\deliveryphase.gif" alt="payment success" class="no-data-found img-fluid" />
+      <div class="row mb-5 d-lg-flex d-none">
+        <div class="col-12 text-center h3 my-green-font">
+          Order Process
+        </div>
+        <div class="col-7 mx-auto text-center mt-0 delivery-img">
+          <img src="image\deliveryphase.gif" alt="payment success" class="no-data-found img-fluid" />
+          <div class="delivery-text-container row d-flex justify-content-around mt-1">
+            <div class="col-4">
+              <span>&nbsp;&nbsp;&nbsp; Place Order</span>
+            </div>
+            <div class="col-4">
+              <span>&nbsp;&nbsp; Payment</span>
+            </div>
+            <div class="col-4">
+              <span>Out For Collection</span>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   </div>
 </body>
 <!-- external script -->
